0d0e5d485a886dc00b9b97a9f6267101b5d82048
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-10-22 09:59:43 -0400

    First 5

---
+++ hello.js
@@ -0,0 +1,30 @@
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('SEI 1019'));
+
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('SEI 1019_two'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('SEI 1019_three'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(('SEI 1019_four'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?'
+    return msg;
+}
+console.log(sayHello('SEI 1019_five'))
